d0c94d18bcc99dcaccb6cb29b6dd30f6aff6b14c
Arman <arman.gh45@gmail.com>
2016-01-23 00:43:09 -0500

    initial commit

---
+++ app/src/main/java/uofthacks2016/atychos/HomePageActivity.java
@@ -0,0 +1,7 @@
+package uofthacks2016.atychos;
+
+/**
+ * Created by armanghassemi on 16-01-22.
+ */
+public class HomePageActivity {
+}
---
+++ app/src/main/java/uofthacks2016/atychos/MainActivity.java
@@ -0,0 +1,78 @@
+package uofthacks2016.atychos;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.thalmic.myo.AbstractDeviceListener;
+import com.thalmic.myo.DeviceListener;
+import com.thalmic.myo.Hub;
+import com.thalmic.myo.Myo;
+import com.thalmic.myo.scanner.ScanActivity;
+
+
+public class MainActivity extends Activity {
+
+
+
+
+    private DeviceListener mListener = new AbstractDeviceListener() {
+        // onConnect() is called whenever a Myo has been connected.
+        @Override
+        public void onConnect(Myo myo, long timestamp) {
+            Toast.makeText(getApplicationContext(), "Myo Connected!", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, HomePageActivity.class);
+            MainActivity.this.startActivity(intent);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        startMyo();
+    }
+
+    void startMyo() {
+        Hub hub = Hub.getInstance();
+        hub.setMyoAttachAllowance(1);
+        if (hub.init(this)) {
+            Log.d("ACCEL", "Initialized Myo");
+            // TODO handle connection errors.
+            hub.attachToAdjacentMyo();
+            hub.addListener(mListener);
+        } else {
+            Log.d("NAO", "Could not initialize the Hub.");
+        }
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
