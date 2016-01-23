f0c9ab42dd1ceb414e4c113b94f73aca6d8818af
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-01-23 10:22:17 -0500

    added Member and User classes

---
+++ app/src/main/java/uofthacks2016/atychos/resources/User.java
@@ -0,0 +1,39 @@
+package uofthacks2016.atychos.resources;
+
+/**
+ * Created by vitaliy on 2016-01-23.
+ */
+public class User {
+
+    public static long NULL_ID = -1;
+
+    // Instance Variables
+    private long id;
+    private String number;
+
+    public User(String number, long id) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public User(String number) {
+        this.id = NULL_ID;
+        this.number = number;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+}
