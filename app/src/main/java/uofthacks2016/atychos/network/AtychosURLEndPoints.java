1d32d8a486b60b4783179b780ae27c6ada125ef7
Vitaliy Krasylovets <krasylovets.vitaliy@gmail.com>
2016-01-23 13:24:52 -0500

    added server end points class

---
+++ app/src/main/java/uofthacks2016/atychos/network/AtychosURLEndPoints.java
@@ -0,0 +1,18 @@
+package uofthacks2016.atychos.network;
+
+/**
+ * Created by vitaliy on 2016-01-23.
+ */
+public class AtychosURLEndPoints {
+    public static final String URL = "https://still-tor-86219.herokuapp.com/";
+
+    //User end points
+    public static final String USER = URL + "/user";
+    public static final String USER_GET = USER + "/get";
+    public static final String USER_GET_MEMBERS = USER + "/getmembers";
+    public static final String USER_SEND_ALERT = USER + "/sendalert";
+
+    //Member end points
+    public static final String MEMBER = URL + "/member";
+    public static final String MEMBER_ADD = MEMBER + "/add";
+}
